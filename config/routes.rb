9a31c368d595b8571ac95c2158a56cc95864edc7
houzairkoussa <houzairmk@icloud.com>
2021-10-26 12:03:11 +0400

    refactored code

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'tasks', to: 'tasks#index'
-  get 'tasks/new', to: 'tasks#new'
-  get 'tasks/:id', to: 'tasks#show', as: :task
-  post 'tasks', to: 'tasks#create'
-  get 'tasks/:id/edit', to: 'tasks#edit'
-  patch 'tasks/:id', to: 'tasks#update'
-  delete 'tasks/:id', to: 'tasks#destroy'
+  # get 'tasks', to: 'tasks#index'
+  # get 'tasks/new', to: 'tasks#new'
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/:id/edit', to: 'tasks#edit'
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
+  resources :tasks
 end
